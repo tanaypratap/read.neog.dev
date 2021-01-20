206888cdd55c5414a1ac3d0a1750c7b58f97b530
Akshay <akshayrajput102@gmail.com>
2021-01-20 22:51:22 +0530

    add mobile menu toggle

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-const menuButton = document.querySelector("#menu");
-const navLinks = document.querySelector("#nav-links");
+// const menuButton = document.querySelector("#menu");
+// const navLinks = document.querySelector("#nav-links");
+
+// function showNav() {
+//     navLinks.classList.toggle('show-nav-links');
+// }
+
+// menuButton.addEventListener("click", showNav);
+
+const menuButton = document.querySelector("btn-menuToggle");
+const navLinks = document.querySelector("#menu");
 
 function showNav() {
     navLinks.classList.toggle('show-nav-links');
